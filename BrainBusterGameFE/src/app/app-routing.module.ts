9c1796b10f730d66cfeb3af8752d7a2651068fa6
Miha Rajšter <rajstermiha@gmail.com>
2023-08-11 23:48:22 +0200

    Routing

--- BrainBusterGameFE/src/app/app-routing.module.ts
+++ BrainBusterGameFE/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from "./pages/about/about.component";
+import {HighscoreComponent} from "./pages/highscore/highscore.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'highscore', component: HighscoreComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
